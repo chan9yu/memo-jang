72407a74c11ec62ce781be49b7d75960bc8f125a
chan9yu <dev.cgyeo@gmail.com>
2025-01-06 00:02:53 +0900

    chore: 의존성 정리 및 package.json 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 3035,
+		port: 3035
 	},
 	define: {
-		APP_VERSION: JSON.stringify(process.env.npm_package_version),
-	},
+		APP_VERSION: JSON.stringify(process.env.npm_package_version)
+	}
 });
